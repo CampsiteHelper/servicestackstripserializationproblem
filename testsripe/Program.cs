eab5504096eac7920784d4f98b7be2843d104919
kpconnell1132 <kevin@connells.net>
2019-04-07 06:44:09 -0500

    initial

---
+++ testsripe/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testsripe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
